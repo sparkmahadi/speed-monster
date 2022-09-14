63a71cb19b007ee484a9d238aad58bb7e0b5c79c
sparkmahadi <mahadi.development@gmail.com>
2022-09-15 00:42:43 +0600

    made final message time integer

--- script.js
+++ script.js
@@ -68,7 +68,7 @@ const gameOver = () => {
   // the current time is the finish time
   // so total time taken is current time - start time
   const finishTime = new Date().getTime();
-  const timeTaken = (finishTime - startTime) / 1000;
+  const timeTaken = parseInt((finishTime - startTime) / 1000);
 
   // show result modal
   resultModal.innerHTML = "";
@@ -111,7 +111,7 @@ const start = () => {
     countdownOverlay.innerHTML = `<h1>${count}</h1>`;
 
     // finished timer
-    if (count == 0) {
+    if (count === 0) {
       // -------------- START TYPING -----------------
       document.addEventListener("keydown", typeController);
       countdownOverlay.style.display = "none";
@@ -120,10 +120,7 @@ const start = () => {
       clearInterval(startCountdown);
       startTime = new Date().getTime();
     }
-    else{
       count--;
-    }
-    console.log(count)
   }, 1000);
 };
 
